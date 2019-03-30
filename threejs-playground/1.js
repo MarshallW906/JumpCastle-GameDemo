907785984635fd2f1f9f38917509bb9d9e5a361a
MarshallW906 <MaticWang@hotmail.com>
2019-03-30 15:51:31 +0800

    0330 webpack study

---
+++ threejs-playground/1.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+// Our Javascript will go here.
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    // animation doesn't require a while-loop, but needs this func instead.
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+if (WEBGL.isWebGLAvailable()) {
+    // Initiate function or other initializations here
+    animate();
+
+} else {
+
+    var warning = WEBGL.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
+
+}
