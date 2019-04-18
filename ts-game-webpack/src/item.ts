6a95e8b0c8c7739c71fdd5dfe1576ba27069ca67
MarshallW906 <MaticWang@hotmail.com>
2019-04-18 14:52:07 +0800

    item basic properties

--- ts-game-webpack/src/item.ts
+++ ts-game-webpack/src/item.ts
@@ -1,9 +1,16 @@
-import { } from './types'
+import { ItemType } from './types'
 
 export class Item {
-
+    id: number;
+    name: string;
+    itemType: ItemType;
+    quantity: number;
+    price: number;
 }
 
 export class ItemFactory {
+    private items = new Array<Item>();
 
+    createNewItem(): void { }
+    destroyItem(): void { }
 }
